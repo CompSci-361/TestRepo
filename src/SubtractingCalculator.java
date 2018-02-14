acca83a35cc10a1db0d35ccc502839c3cabb5448
adamdunn <adamdunn@uwm.edu>
2018-02-13 23:08:58 -0600

    uhm, i think it works

--- src/SubtractingCalculator.java
+++ src/SubtractingCalculator.java
@@ -4,7 +4,7 @@ public class SubtractingCalculator implements ICalculator {
 	@Override
 	public double Calculate(double left, double right) {
 		// TODO Auto-generated method stub
-		return 0;
+		return left-right;
 	}
 
 }
